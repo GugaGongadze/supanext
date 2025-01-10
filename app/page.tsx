97e78c148e428cfaeb3e88c0a39aecdcacb5e75f
Guga Gongadze <gugongadze@gmail.com>
2025-01-10 20:45:58 +0400

    change site header

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Index() {
   return (
     <>
       <Hero />
-      <h1>შინ წავიდეთ</h1>
+      <h1>შინ წავიდეთ!</h1>
       <input type="checkbox" />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
